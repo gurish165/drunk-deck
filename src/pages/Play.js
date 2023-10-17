182204e8a340c12b2d8b44d3d5234df7f2ba0264
Gurish Sharma <gurish@Gurishs-MacBook-Pro.local>
2023-10-17 18:12:09 -0400

    fixed random color logic

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -8,16 +8,21 @@ function Play() {
     const { players, questions } = useContext(PlayersContext);
     const [currentQuestion, setCurrentQuestion] = useState('');
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [backgroundColor, setBackgroundColor] = useState('#29bf00'); // New state for the background color
+    const [backgroundColor, setBackgroundColor] = useState('#29bf00');
+    const [prevBackgroundColor, setPrevBackgroundColor] = useState('');  // New state for previous color
     const history = useNavigate();
-
-    // Define the list of hex colors
     const colors = ['#fa0000', '#faa300', '#faa300', '#29bf00', '#0285f7', '#2802fa', '#af03ff', '#ed05b7'];
+    
 
     // Function to get a random color from the colors list
     const getRandomColor = () => {
-        const randomIndex = Math.floor(Math.random() * colors.length);
-        return colors[randomIndex];
+        let randomColor;
+        do {
+            const randomIndex = Math.floor(Math.random() * colors.length);
+            randomColor = colors[randomIndex];
+        } while (randomColor === prevBackgroundColor);  // Ensure it's not the same as previous color
+
+        return randomColor;
     };
 
     const nextQuestion = () => {
@@ -25,7 +30,9 @@ function Play() {
             setCurrentIndex(prevIndex => {
                 const nextIndex = prevIndex + 1;
                 setCurrentQuestion(questions[nextIndex][0]);
-                setBackgroundColor(getRandomColor()); // Set a random color when the user clicks Next
+                const newBackgroundColor = getRandomColor();
+                setPrevBackgroundColor(backgroundColor);  // Update the previous color before setting a new one
+                setBackgroundColor(newBackgroundColor);
                 return nextIndex;
             });
         }
@@ -36,7 +43,9 @@ function Play() {
             setCurrentIndex(prevIndex => {
                 const previousIndex = prevIndex - 1;
                 setCurrentQuestion(questions[previousIndex][0]);
-                setBackgroundColor(getRandomColor()); // Set a random color when the user clicks Prev
+                const newBackgroundColor = getRandomColor();
+                setPrevBackgroundColor(backgroundColor);  // Update the previous color before setting a new one
+                setBackgroundColor(newBackgroundColor);
                 return previousIndex;
             });
         }
